6378dda0a3ebb1c5d32b02f368c359372d92bade
karthikshetty <karthikashetty036@gmail.com>
2026-05-19 10:19:13 +0530

    Create prg4.c

---
+++ prg4.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+
+#define INF 999
+
+void dij(int n, int v, int cost[10][10], int dist[10])
+{
+    int i, w, u, count, min;
+    int flag[10] = {0};
+
+    // initialize distances from source
+    for(i = 1; i <= n; i++)
+    {
+        dist[i] = cost[v][i];
+    }
+
+    flag[v] = 1;
+    count = 2;
+
+    while(count <= n)
+    {
+        min = INF;
+
+        // pick minimum distance unvisited node
+        for(w = 1; w <= n; w++)
+        {
+            if(dist[w] < min && !flag[w])
+            {
+                min = dist[w];
+                u = w;
+            }
+        }
+
+        flag[u] = 1;
+        count++;
+
+        // relax edges
+        for(w = 1; w <= n; w++)
+        {
+            if(!flag[w] && dist[u] + cost[u][w] < dist[w])
+            {
+                dist[w] = dist[u] + cost[u][w];
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n, v, i, j;
+    int cost[10][10], dist[10];
+
+    printf("\n Enter the number of nodes:");
+    scanf("%d", &n);
+
+    printf("\n Enter the cost matrix:\n");
+
+    for(i = 1; i <= n; i++)
+    {
+        for(j = 1; j <= n; j++)
+        {
+            scanf("%d", &cost[i][j]);
+
+            if(cost[i][j] == 0)
+                cost[i][j] = INF;
+        }
+    }
+
+    printf("\n Enter the source matrix:");
+    scanf("%d", &v);
+
+    dij(n, v, cost, dist);
+
+    printf("\n Shortest path:\n");
+
+    for(i = 1; i <= n; i++)
+    {
+        if(i != v)
+        {
+            printf("%d->%d,cost=%d\n", v, i, dist[i]);
+        }
+    }
+
+    return 0;
+}
