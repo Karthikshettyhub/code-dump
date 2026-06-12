148d400856839a861dd7fb9de01a0230a62b5d7c
karthikshetty <karthikashetty036@gmail.com>
2026-06-12 22:25:27 +0530

    Create prg8.c

---
+++ prg8.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#define MAX 10
+
+int set[MAX];
+int chosen[MAX];
+int n, target;
+
+void findSubsets(int pos, int currentSum)
+{
+    // Base case: checked all elements
+    if (pos == n + 1)
+    {
+        if (currentSum == target)
+        {
+            for (int i = 1; i <= n; i++)
+                if (chosen[i] == 1)
+                    printf("%d ", set[i]);
+            printf("\n");
+        }
+        return;
+    }
+
+    // Choice 1: INCLUDE set[pos]
+    chosen[pos] = 1;
+    findSubsets(pos + 1, currentSum + set[pos]);
+
+    // Choice 2: EXCLUDE set[pos]
+    chosen[pos] = 0;
+    findSubsets(pos + 1, currentSum);
+}
+
+int main()
+{
+    int totalSum = 0;
+
+    printf("\nEnter the n value:");
+    scanf("%d", &n);
+
+    printf("\nEnter the set in increasing order:");
+    for (int i = 1; i <= n; i++)
+        scanf("%d", &set[i]);
+
+    printf("\nEnter the max subset value:");
+    scanf("%d", &target);
+
+    for (int i = 1; i <= n; i++)
+        totalSum += set[i];
+
+    if (totalSum < target || set[1] > target)
+        printf("\nNo subset possible");
+    else
+        findSubsets(1, 0);
+
+    return 0;
+}
