055047f3df1f11578d1dae032f2844e8ec724866
karthikshetty <karthikashetty036@gmail.com>
2026-07-15 11:14:51 +0530

    Update prg8.c

--- prg8.c
+++ prg8.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #define MAX 10
-
 int set[MAX];
 int chosen[MAX];
 int n, target;
@@ -8,22 +7,20 @@ int n, target;
 void findSubsets(int pos, int currentSum)
 {
     // Base case: checked all elements
-    if (pos == n + 1)
+    if (pos == n)
     {
         if (currentSum == target)
         {
-            for (int i = 1; i <= n; i++)
+            for (int i = 0; i < n; i++)
                 if (chosen[i] == 1)
                     printf("%d ", set[i]);
             printf("\n");
         }
         return;
     }
-
     // Choice 1: INCLUDE set[pos]
     chosen[pos] = 1;
     findSubsets(pos + 1, currentSum + set[pos]);
-
     // Choice 2: EXCLUDE set[pos]
     chosen[pos] = 0;
     findSubsets(pos + 1, currentSum);
@@ -32,24 +29,18 @@ void findSubsets(int pos, int currentSum)
 int main()
 {
     int totalSum = 0;
-
     printf("\nEnter the n value:");
     scanf("%d", &n);
-
     printf("\nEnter the set in increasing order:");
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n; i++)
         scanf("%d", &set[i]);
-
     printf("\nEnter the max subset value:");
     scanf("%d", &target);
-
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n; i++)
         totalSum += set[i];
-
-    if (totalSum < target || set[1] > target)
+    if (totalSum < target || set[0] > target)
         printf("\nNo subset possible");
     else
-        findSubsets(1, 0);
-
+        findSubsets(0, 0);
     return 0;
 }
